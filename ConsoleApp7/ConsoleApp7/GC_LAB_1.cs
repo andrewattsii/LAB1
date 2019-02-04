14448606a753be51e3f2e4c64298dc345e5b470b
andrewattsii <43122582+andrewattsii@users.noreply.github.com>
2019-02-03 22:10:00 -0500

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/GC_LAB_1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace GC_LAB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+
+
+                Console.WriteLine("Enter three numbers :");
+                string inputOne = Console.ReadLine();
+                Double num1 = Convert.ToDouble(inputOne);
+
+
+                string inputTwo = Console.ReadLine();
+                Double num2 = Convert.ToDouble(inputTwo);
+
+                string inputThree = Console.ReadLine();
+                Double num3 = Convert.ToDouble(inputThree);
+
+
+                var total = num1 + num2 + num3;
+                var average = total / 3;
+
+
+                Console.WriteLine("Total:" + total);
+                Console.WriteLine("Average:" + average);
+
+
+                Double[] Double = new Double[] { num1, num2, num3 };
+                Console.WriteLine(Double.Max());
+                Console.WriteLine(Double.Min());
+
+
+                Console.WriteLine("Us:" + total.ToString("C"));
+                Console.WriteLine("Sweedish:" + total.ToString("C", new CultureInfo("sv-SE")));
+                Console.WriteLine("Japanese:" + total.ToString("C", new CultureInfo("ja-JP")));
+                Console.WriteLine("Thai" + total.ToString("C", new CultureInfo("th-TH")));
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
